96620ce8b5c938587d9c026e6e5ce0f8f9b09ead
Ami-thakor <128568711+Ami-thakor@users.noreply.github.com>
2025-07-24 10:51:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def output(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run()
